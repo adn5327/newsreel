734bf0dbd239232530c7c2a2afee7001d725ba03
Abhishek Deep Nigam <adn5327@gmail.com>
2016-04-07 12:21:00 -0500

    adding nytimes article search

--- newsreel/centerpiece/nytimes.py
+++ newsreel/centerpiece/nytimes.py
@@ -1,7 +1,18 @@
 import requests, json
 import keys
-def article_search():
-    pass
+def article_search(query):
+    endpoint = 'http://api.nytimes.com/svc/search/v2/articlesearch.json'
+    #q=bernie+sanders&sort=newest&page=1&api-key=sample-key
+    payload = {'q':query,'sort':'newest','page':1,'api-key':keys.nytsearchkey} 
+    r = requests.get(endpoint,params=payload) 
+    json_vals = r.json()
+    response = json_vals['response']
+    for each in response['docs']:
+        print '------------'
+        print each['headline']['main']
+        print each['web_url']+'\n'
+        print each['lead_paragraph']
+        print '------------\n'
 def top_stories(section):
     endpoint = 'http://api.nytimes.com/svc/topstories/v1/' 
     endpoint += section
@@ -19,5 +30,5 @@ def top_stories(section):
         print '----------------'
         print cur['title'] + ', ' + cur['url'] 
 if __name__=="__main__":
-    article_search()
+    article_search('israel')
     top_stories('world')
