cf470422fc3a5dcd2624754a5ca57dd438f46b19
Abhishek Deep Nigam <adn5327@gmail.com>
2016-04-21 02:05:48 -0500

    work towards making multiple distinct UIs -- code still needs to be cleaned up

--- centerpiece/urls.py
+++ centerpiece/urls.py
@@ -5,5 +5,10 @@ app_name = 'centerpiece'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^world/$', views.world_top, name='world'),
+    url(r'^middleeast/$', views.middle_east_top, name='middleeast'),
+    url(r'^europe/$', views.europe_top, name='europe'),
+    url(r'^asia/$', views.asia_top, name='asia'),
+    url(r'^usa/$', views.usa_top, name='usa'),
     url(r'^africa/$', views.africa_top, name='africa'),
+	
 ]
--- centerpiece/views.py
+++ centerpiece/views.py
@@ -22,3 +22,35 @@ def africa_top(request):
     guardian.guardian_search('africa',psize=2,listy=cur_list)
     context = {'title':'AFRICA','top_stories':top_stories, 'stlist': cur_list} 
     return render(request, 'centerpiece/world.html', context)
+def usa_top(request):
+    top_stories = nytimes.top_stories('politics')
+    cur_list =[]
+    npr.npr_search('usa',numResults=2,listy=cur_list)
+    guardian.guardian_search('usa',psize=2,listy=cur_list)
+    context = {'title':'USA','top_stories':top_stories, 'stlist': cur_list} 
+    return render(request, 'centerpiece/world.html', context)
+def middle_east_top(request):
+    top_stories = nytimes.top_stories('world')
+    cur_list =[]
+    #npr.npr_search('middle east',numResults=2,listy=cur_list)
+    #guardian.guardian_search('middle east',psize=2,listy=cur_list)
+    iran=[]
+    israel=[]
+    #iran = guardian.guardian_search('iran',psize=2)
+    #israel = npr.npr_search('israel',numResults=2)
+    context = {'title':'MIDDLE EAST','top_stories':top_stories, 'stlist': cur_list, 'iran':iran, 'israel':israel}
+    return render(request, 'centerpiece/middle_east.html', context)
+def europe_top(request):
+    top_stories = nytimes.top_stories('world')
+    cur_list =[]
+    npr.npr_search('europe',numResults=2,listy=cur_list)
+    guardian.guardian_search('europe',psize=2,listy=cur_list)
+    context = {'title':'EUROPE','top_stories':top_stories, 'stlist': cur_list} 
+    return render(request, 'centerpiece/world.html', context)
+def asia_top(request):
+    top_stories = nytimes.top_stories('world')
+    cur_list =[]
+    npr.npr_search('asia',numResults=2,listy=cur_list)
+    guardian.guardian_search('asia',psize=2,listy=cur_list)
+    context = {'title':'ASIA','top_stories':top_stories, 'stlist': cur_list} 
+    return render(request, 'centerpiece/world.html', context)
